b7ea79778714af94dae612c07a83be13425f5ad5
Kiefer Hagin <kieferhagin@gmail.com>
2015-10-23 12:25:43 -0700

    update config

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt){
                     variables: {
                         'cssFileUrl': 'https://s3.amazonaws.com/<%= deployBuckets.prod %>/knock-integration-<%= pkg.version %>.min.css',
                         'companyHost': 'https://knockrentals.com',
-                        'schedulingHost': 'https://knockrentals.github.io/Prod-WebSchedule/#'
+                        'schedulingHost': 'https://knockrentals.github.io/schedule/#'
                     }
                 }
             },
